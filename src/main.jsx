e8dae555bad079931cf16c82a6a4b452625ea197
Francis Gael <francisgaelg@gmail.com>
2026-05-02 13:46:57 +0800

    restart

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 // Main App
+import './index.css'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
